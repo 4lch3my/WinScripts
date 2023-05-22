cb9fc6bb4b94258afc85f60b439212e44026e011
4lch3my <fordit1517@gmail.com>
2023-05-22 23:10:23 +0200

    Create cred_clear.bat

---
+++ cred_clear.bat
@@ -0,0 +1,10 @@
+:: Windows 10 Credential Manager Clearer for IT environment
+:: Simply run as administrator and all of your credentials will be cleared from your machine
+
+@echo off
+cmdkey.exe /list > "%TEMP%\List.txt"
+findstr.exe Target "%TEMP%\List.txt" > "%TEMP%\tokensonly.txt"
+FOR /F "tokens=1,2 delims= " %%G IN (%TEMP%\tokensonly.txt) DO cmdkey.exe /delete:%%H
+del "%TEMP%\List.txt" /s /f /q
+del "%TEMP%\tokensonly.txt" /s /f /q
+exit
